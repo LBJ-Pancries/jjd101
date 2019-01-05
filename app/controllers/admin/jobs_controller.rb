e0f131631bbaeafcfdb49e50e9ae28f0fd64268c
liubingjie <breeder1121@gmail.com>
2019-01-05 08:53:11 +0800

    create wage_upper_bound wage_lower_bound contact_email is_hidden to job

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -8,7 +8,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.all
+    @jobs = Job.where(:is_hidden => true).order("created_at DESC")
   end
 
   def new
@@ -46,6 +46,6 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
 end
